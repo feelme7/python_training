0bc8068c21b5a3ffbf99c0ed183b7d7ab7bb1ce2
oflpn <o.flpnk@gmail.com>
2022-07-18 22:01:08 +0300

    были удалены лишние строки кода

--- test_add_group.py
+++ test_add_group.py
@@ -7,30 +7,28 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        #ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
-        #ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_local | ]]
-        #ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
-        wd.close()
-        #ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_local | ]]
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_local | ]]
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_local | ]]
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_xpath("//body").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("dfg")
         wd.find_element_by_name("group_header").click()
@@ -42,18 +40,21 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
-    
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
     def tearDown(self):
         self.wd.quit()
 
