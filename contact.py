8227ff6d4668210c3392bac51ee17d7f9b3cf92d
oflpn <o.flpnk@gmail.com>
2022-07-26 19:51:06 +0300

    add helper class

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.mobile = mobile
+        self.email = email
