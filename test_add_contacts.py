059a237775a9bbfd1a75166799248f1d1bddb86d
oflpn <o.flpnk@gmail.com>
2022-07-26 19:53:25 +0300

    use helper class

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
 
 
 def open_home_page(wd):
@@ -23,22 +24,22 @@ def open_add_new_page(wd):
     wd.find_element_by_link_text("add new").click()
 
 
-def create_contact(wd, firstname, middlename, lastname, mobile, email):
+def create_contact(wd, contact):
     wd.find_element_by_name("firstname").click()
     wd.find_element_by_name("firstname").clear()
-    wd.find_element_by_name("firstname").send_keys(firstname)
+    wd.find_element_by_name("firstname").send_keys(contact.firstname)
     wd.find_element_by_name("middlename").click()
     wd.find_element_by_name("middlename").clear()
-    wd.find_element_by_name("middlename").send_keys(middlename)
+    wd.find_element_by_name("middlename").send_keys(contact.middlename)
     wd.find_element_by_name("lastname").click()
     wd.find_element_by_name("lastname").clear()
-    wd.find_element_by_name("lastname").send_keys(lastname)
+    wd.find_element_by_name("lastname").send_keys(contact.lastname)
     wd.find_element_by_name("mobile").click()
     wd.find_element_by_name("mobile").clear()
-    wd.find_element_by_name("mobile").send_keys(mobile)
+    wd.find_element_by_name("mobile").send_keys(contact.mobile)
     wd.find_element_by_name("email").click()
     wd.find_element_by_name("email").clear()
-    wd.find_element_by_name("email").send_keys(email)
+    wd.find_element_by_name("email").send_keys(contact.email)
     # enter contact
     wd.find_element_by_name("theform").click()
     wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
@@ -58,8 +59,8 @@ class TestAddContacts(unittest.TestCase):
         open_home_page(wd)
         login(wd, username="admin", password="secret")
         open_add_new_page(wd)
-        create_contact(wd, firstname="John", middlename="Christopher", lastname="Depp", mobile="89993332211",
-                       email="jcd@gmail.com")
+        create_contact(wd, Contact(firstname="John", middlename="Christopher", lastname="Depp", mobile="89993332211",
+                       email="jcd@gmail.com"))
         logout(wd)
 
     def test_add_empty_contacts(self):
@@ -67,19 +68,19 @@ class TestAddContacts(unittest.TestCase):
         open_home_page(wd)
         login(wd, username="admin", password="secret")
         open_add_new_page(wd)
-        create_contact(wd, firstname="", middlename="", lastname="", mobile="", email="")
+        create_contact(wd, Contact(firstname="", middlename="", lastname="", mobile="", email=""))
         logout(wd)
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
     def tearDown(self):
         self.wd.quit()
 
