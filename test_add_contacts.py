19f3814c4225cd243d8f219d70a7736fd0b28cd0
oflpn <o.flpnk@gmail.com>
2022-07-26 15:48:45 +0300

    delete unnecessary lines

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -7,14 +7,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r'')
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+
     def test_untitled_test_case(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/")
@@ -62,20 +60,8 @@ class UntitledTestCase(unittest.TestCase):
         except NoAlertPresentException as e: return False
         return True
     
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
