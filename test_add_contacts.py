2a0be49b5a788d8147f400ec82a905fc2bad82f2
oflpn <o.flpnk@gmail.com>
2022-07-31 21:51:47 +0300

    add pytest and functions for creating a fixture

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from contact import Contact
 from application_contacts import Application
 
 
-class TestAddContacts(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contacts(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="John", middlename="Christopher", lastname="Depp",
-                                        mobile="89993332211", email="jcd@gmail.com"))
-        self.app.logout()
-
-    def test_add_empty_contacts(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="", middlename="", lastname="", mobile="", email=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contacts(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="John", middlename="Christopher", lastname="Depp",
+                               mobile="89993332211", email="jcd@gmail.com"))
+    app.logout()
+
+
+def test_add_empty_contacts(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="", middlename="", lastname="", mobile="", email=""))
+    app.logout()
