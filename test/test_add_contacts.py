7fc14d1f3fa4d53836c8eab9e71502fcd76c355a
oflpn <o.flpnk@gmail.com>
2022-08-01 20:09:32 +0300

    add packages with helper classes for contact and group

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_contacts(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="John", middlename="Christopher", lastname="Depp",
+    app.contact.create(Contact(firstname="John", middlename="Christopher", lastname="Depp",
                                mobile="89993332211", email="jcd@gmail.com"))
     app.session.logout()
 
 
 def test_add_empty_contacts(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="", mobile="", email=""))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", mobile="", email=""))
     app.session.logout()
